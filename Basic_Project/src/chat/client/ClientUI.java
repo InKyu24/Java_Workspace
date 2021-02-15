f8a5d634a8335543c6ca0e35ded79f0dd0376ed4
InKyu CHOI <484342@gmail.com>
2021-02-15 14:06:21 +0900

    Create Inner class for thread

--- Basic_Project/src/chat/client/ClientUI.java
+++ Basic_Project/src/chat/client/ClientUI.java
@@ -2,50 +2,60 @@ package chat.client;
 
 import java.awt.*;
 import java.awt.event.*;
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.net.Socket;
-import java.net.UnknownHostException;
+import java.io.*;
+import java.net.*;
 
 public class ClientUI {
 	TextArea ta;
 	TextField tf;
 	DataOutputStream out;
+	DataInputStream in;
+	String chatId;
+	
+	class ClientThread extends Thread{				// inner class 생성
+		@Override
+		public void run() {
+			while(true) {
+				try {
+					ta.append(in.readUTF()+"\n");	// 채팅 메세지 읽기 (반복)
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 
-	public void chatMsg() { 				// Inner class
+	public void chatMsg() { 				
 		String msg=tf.getText();
 		try {
-			out.writeUTF(msg);
+			out.writeUTF(chatId + msg);				// 채팅 메세지 보내기
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		tf.setText("");
 	}
 	
-	public void onCreate() { 				// Local class
-		Frame f = new Frame("나의 채팅"); 		// 제목있는 프레임 생성
-		Button b1 = new Button("Send"); 	// 문구 들어간 버튼 생성
-		Panel p=new Panel();				// 패널 생성
-		tf = new TextField(20); 			// 길이 20 TextField 생성
-		ta = new TextArea(); 				// TextArea 생성
+	public void onCreate() { 						// Local class
+		Frame f = new Frame("나의 채팅"); 				// 제목있는 프레임 생성
+		Button b1 = new Button("join"); 			// 문구 들어간 버튼 생성
+		Button b2 = new Button("send");
+		Panel p=new Panel();						// 패널 생성
+		tf = new TextField(20); 					// 길이 20 TextField 생성
+		ta = new TextArea(); 						// TextArea 생성
 
-		MenuBar mb = new MenuBar();			// Menubar 생성
-		Menu file_menu = new Menu("File");	// 파일 Menu 생성
-		Menu edit_menu = new Menu("Edit");	// 편집 Menu 생성
-		MenuItem open_item = new MenuItem("Open"); // 열기  MenuItem 생성
-		MenuItem save_item = new MenuItem("Save"); // 저장 MenuItem 생성
+		MenuBar mb = new MenuBar();					// Menubar 생성
+		Menu file_menu = new Menu("File");			// 파일 Menu 생성
+		Menu edit_menu = new Menu("Edit");			// 편집 Menu 생성
+		MenuItem open_item = new MenuItem("Open");	 // 열기  MenuItem 생성
+		MenuItem save_item = new MenuItem("Save");	 // 저장 MenuItem 생성
 		MenuItem close_item = new MenuItem("Close"); // 닫기 MenuItem 생성
 		
-		file_menu.add(open_item);			// 파일 Menu에 열기 MenuItem 추가 
-		file_menu.add(save_item);			// 파일 Menu에 저장 MenuItem 추가
-		file_menu.add(close_item);			// 파일 Menu에 닫기 MenuItem 추가
-		mb.add(file_menu);					// Menubar에 파일 Menu 추가
-		mb.add(edit_menu);					// Menubar에 편집 Menu 추가
-		f.setMenuBar(mb);					// 프레임에 Menubar 생성
+		file_menu.add(open_item);					// 파일 Menu에 열기 MenuItem 추가 
+		file_menu.add(save_item);					// 파일 Menu에 저장 MenuItem 추가
+		file_menu.add(close_item);					// 파일 Menu에 닫기 MenuItem 추가
+		mb.add(file_menu);							// Menubar에 파일 Menu 추가
+		mb.add(edit_menu);							// Menubar에 편집 Menu 추가
+		f.setMenuBar(mb);							// 프레임에 Menubar 생성
 
 		save_item.addActionListener(new ActionListener() {		// 저장 MenuItem 행동 메소드 설정
 			@Override
@@ -58,13 +68,11 @@ public class ClientUI {
 					fw=new FileWriter(save.getDirectory()+save.getFile());
 					fw.write(ta.getText());	
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}finally {
 					 try {
 						 if(fw !=null ) fw.close();
 					} catch (IOException e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 				}		
@@ -137,13 +145,17 @@ public class ClientUI {
 		b1.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {			// 채팅 서버 연결
-				String chatId=tf.getText();
+				chatId="["+tf.getText()+"]";
 				ta.setText(chatId+"님 채팅을 시작합니다\n");
-				tf.setText("");
 				try {
 					Socket s = new Socket("localhost", 9999);
-					ta.append("연결 OK\n");
 					out=new DataOutputStream(s.getOutputStream());
+					in=new DataInputStream(s.getInputStream());
+					ClientThread t = new ClientThread();
+					t.start();
+					ta.append("연결 OK\n");
+					tf.setText("");
+					
 				} catch (UnknownHostException e1) {				
 					e1.printStackTrace();
 				} catch (IOException e1) {				
@@ -161,6 +173,14 @@ public class ClientUI {
 			}
 		});
 		
+		b2.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				chatMsg();				
+			}
+		});
+		
 		// Anonymous Local class : Inner class의 내용이 하나의 메소드에서만 사용할 때 클래스 선언과 객체화를 동시에 하는 것
 		// WindowAdapter는 abstract 클래스로 생성자 호출이 불가하지만 FrameHandler 클래스를 익명의 클래스로 하여 호출을 한 것이라 볼 수 있다.
 		//	FlowLayout mgr=new FlowLayout();
@@ -171,6 +191,7 @@ public class ClientUI {
 		f.add(p,BorderLayout.SOUTH);
 		p.add(tf);
 		p.add(b1);
+		p.add(b2);
 		
 		p.setBackground(Color.gray);
 		
@@ -185,6 +206,6 @@ public class ClientUI {
 		public static void main(String[] args) {
 			ClientUI ui=new ClientUI();
 			ui.onCreate();
-		}//end main
+		}														//end main
 
-}//end ClientUi
+}																//end ClientUi
--- Basic_Project/src/chat/server/Server.java
+++ Basic_Project/src/chat/server/Server.java
@@ -2,17 +2,36 @@ package chat.server;
 
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+
+import javax.swing.plaf.synth.SynthSpinnerUI;
 
 public class Server {
+	ArrayList<DataOutputStream> allClientOutput=new ArrayList<DataOutputStream>();
+	
+	public void broadcast (String msg) {
+		synchronized (allClientOutput) {
+			for (DataOutputStream out:allClientOutput) {
+				try {
+					out.writeUTF(msg);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
 	public void chatProcess() {
 		try {
-			ServerSocket ss=new ServerSocket(9999); //0~65565 1024번까지는 시스템 포트 21:FTP 23:Telnet 25:SMTP 80:HTTP
+			ServerSocket ss=new ServerSocket(9999); 					// 0~65565 1024번까지는 시스템 포트 21:FTP 23:Telnet 25:SMTP 80:HTTP
 			System.out.println("server ready");
 			while(true) {
 				Socket s = ss.accept();
 				System.out.println(s.getInetAddress()+"님 접속");
 				DataInputStream in = new DataInputStream(s.getInputStream());
-				ServerThread t = new ServerThread(in);
+				DataOutputStream out = new DataOutputStream(s.getOutputStream());
+				allClientOutput.add(out);
+				ServerThread t = new ServerThread(in,out);
 				t.start();
 			}
 		} catch (IOException e) {
@@ -24,16 +43,19 @@ public class Server {
 		new Server().chatProcess();
 	} // end main
 	
-	class ServerThread extends Thread {			// inner class 생성
+	class ServerThread extends Thread {								// inner class 생성
 		DataInputStream in;
-		public ServerThread(DataInputStream in) {
+		DataOutputStream out;
+		public ServerThread(DataInputStream in, DataOutputStream out) {
 			this.in=in;
+			this.out=out;
 		}
 		@Override
 		public void run() {
 			String msg="";
 			try {
-			msg = in.readUTF();
+				msg = in.readUTF();
+				broadcast(msg);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
