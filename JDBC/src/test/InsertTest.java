8b9ffbf8c0f3c4fd15377ba908216238586b7286
InKyu <484342@gmail.com>
2021-02-18 17:19:24 +0900

    Practice INSERT statement through JDBC

--- JDBC/src/test/InsertTest.java
+++ JDBC/src/test/InsertTest.java
@@ -1,15 +1,16 @@
 package test;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class InsertTest {
 	public static void main(String[] args) {
 		Connection con = null;
-		Statement stmt =null;
+		PreparedStatement stmt =null;
 		ResultSet rs = null;
 		
 		try {	
@@ -18,20 +19,27 @@ public class InsertTest {
 			System.out.println("Loading Success");
 		
 		// 2. Connection
-			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "SHOP", "1313");
+			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "CAFE", "1313");
 			System.out.println("Connection Success");
-		
+
 		// 3. Statement
-			stmt = con.createStatement();
+ 			stmt = con.prepareStatement("insert into member values(?,?,?,?,?)"); 
+ 			stmt.setString(1,args[0]);
+			stmt.setString(2,args[1]);
+			java.util.Date today = new java.util.Date();
+			Date now = new Date(today.getTime());
+			stmt.setDate(3, now);
+			stmt.setString(4,args[2]);
+			stmt.setInt(5,0);
 			System.out.println("Statement Success");
 			
 		// 4. SQL 전송
-			int i = stmt.executeUpdate("Insert into memberTBL values ('Choi','최인규','서울 성북구 돈암2동') ");
-			
+			int i = stmt.executeUpdate();
 			
 		// 5. 결과 얻기
 			System.out.println("This is the Result");
 			System.out.println(i+"행이 Insert되었습니다.");
+			
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
@@ -49,3 +57,4 @@ public class InsertTest {
 		}
 	}
 }
+
