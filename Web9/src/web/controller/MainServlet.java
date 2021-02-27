03b36159a9ac08e2ae9cbb1ef563fa35c9e9d88c
InKyu <484342@gmail.com>
2021-02-28 00:31:10 +0900

    Eliminate unnecessary import statements

--- Web9/src/web/controller/MainServlet.java
+++ Web9/src/web/controller/MainServlet.java
@@ -9,8 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import sun.misc.Signal;
-
 
 @WebServlet("/main")
 public class MainServlet extends HttpServlet {
