a7b8e141d8e94721e4b3efb98c9c0fb7d5030be0
InKyu <484342@gmail.com>
2021-03-11 16:55:45 +0900

    Correct typos

--- SpringTest06_TX/src/web/controller/AccountControllerImpl.java
+++ SpringTest06_TX/src/web/controller/AccountControllerImpl.java
@@ -8,12 +8,13 @@ import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
 import web.service.AccountService;
 
+
 public class AccountControllerImpl extends MultiActionController implements AccountController {
 	AccountService accountService;
 	
 	// 속성 accountService에 빈을 주입하기 위해 setter 구현
-	public void setAccountservice(AccountService accountservice) {
-		this.accountService = accountservice;
+	public void setAccountService(AccountService accountService) {
+		this.accountService = accountService;
 	}
 	
 	@Override
--- SpringTest06_TX/src/web/dao/AccountDAOImpl.java
+++ SpringTest06_TX/src/web/dao/AccountDAOImpl.java
@@ -14,14 +14,14 @@ public class AccountDAOImpl implements AccountDAO {
 	// 첫 번째 update문을 실행해 홍길동 계좌에서 출금
 	@Override
 	public void updateBalance1() {
-		sqlSession.update("mapper.accout.updateBalance1");
+		sqlSession.update("mapper.account.updateBalance1");
 		
 	}
 
 	// 두 번째 update문을 실행해 김유신 계좌에 입금
 	@Override
 	public void updateBalance2() {
-		sqlSession.update("mapper.accout.updateBalance2");
+		sqlSession.update("mapper.account.updateBalance2");
 		
 	}
 
