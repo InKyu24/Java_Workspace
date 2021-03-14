d2bfd8145e4acb5a1b243dd9f79b4583195e0677
InKyu <484342@gmail.com>
2021-03-14 17:07:47 +0900

    Complete STS Assignment02

--- STS_Assignment02/WebContent/test.js
+++ STS_Assignment02/WebContent/test.js
@@ -7,55 +7,31 @@ $(document).ready(function () {
 		alert('AI 기술 사이트로 이동하시겠습니까?');
 		$('#main').html('<h1>AI 기술 사이트</h1>');
 	});
-	
-/*	$('#login').click(function (){
-		$.post(
-			"/",
-			
-			JSON.stringify({
-					sign:"login",
-					id:$("#id").val(),
-					pw:$("#pw").val()
-		    }),
-		    
-		    function(msg,status){
-					var obj=JSON.parse(msg);
-					$("#msgDiv").html(obj.id+"님 환영합니다");
-					$('#loginForm').html("<input id='login' type='submit' value='LOGOUT'>");
-					$('#member').html("MY ID : "+id);
-			});
-	});*/
-			
-/*	$('#login').click(function (){	
-		var id = $('#id').val();
-		var pw = $('#pw').val();
-		alert(id+':'+pw);{
-		$.ajax({
-				type: 'post',
-				success:function(){
-					alert("Data: "+id+"님 login ok\nStatus: success");
-				}
-			});		
-		}
-		$('#loginForm').html("<input id='login' type='submit' value='LOGOUT'>");
-		$('#member').html("MY ID : "+id);
-	});*/
-	
 	$('#login').click(function (){
-		var id = $('#id').val();
-		var pw = $('#pw').val();
-		$.ajax({			
-				type: 'post',
-				success:function(){
-					alert(id+':'+pw);
-				},
-				complete:function(){
-					alert("Data: "+id+"님 login ok\nStatus: success");
-					$('#loginForm').html("<input id='login' type='submit' value='LOGOUT'>");
-					$('#member').html("MY ID : "+id);
-				}
-			});		
+		var _id = $('#id').val();
+		if (_id == '') {
+			alert ("ID를 입력하세요");
+			return;
+		}
+		var _pw = $('#pw').val();
+		if (_pw == '') {
+			alert ("PW를 입력하세요");
+			return;
+		}
+		alert(_id+':'+_pw);
+		
+		$.ajax({
+			type : "post",
+			url: "http://localhost:8383/STS_Assignment02/login",
+			data : {id: _id, pw: _pw},
+			success : function (data, status) {
+				alert(data+status);
+			},
+			error: function () {
+				alert("에러 발생");
+			}
+		});
+		$('#loginForm').html("<input id='login' type='submit' value='LOGOUT'>");
+		$('#member').html("MY ID : "+_id);
 	});
-
-	
 });
--- STS_Assignment02/src/web/controller/LoginController.java
+++ STS_Assignment02/src/web/controller/LoginController.java
@@ -1,53 +1,31 @@
 package web.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class LoginController
- */
-@WebServlet("/")
+
+@WebServlet("/login")
 public class LoginController extends HttpServlet {
-	public LoginController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("utf-8");
-		String sign = request.getParameter("sign");
-		
-		if (sign == null) {
-			return;
-		} else if (sign.equals("login")) {
-			BufferedReader br = request.getReader();
-			JSONObject jsonObject = (JSONObject) JSONValue.parse(br);
+			request.setCharacterEncoding("utf-8");
+			response.setContentType("text/html; charset=utf-8");
 			
-			String sign = (String) jsonObject.get("sign");
-			String id=(String) jsonObject.get("id");
-			String pw=(String) jsonObject.get("pw");
+			String id = request.getParameter("id");
+			String pw = request.getParameter("pw");
+			System.out.println("id : "+id+"\n"+"pw : "+pw);
 			
-			JSONObject returnObject = new JSONObject ();
-			returnObject.put("id", id);
-			returnObject.put("pw", pw);
-			
-			HttpSession session = request.getSession();
-			session.setAttribute("id", id);
-			
-			String returnString=returnObject.toJSONString();
-			
-			PrintWriter out=response.getWriter();
-			out.append(returnString);
-			
-		}		
-			
+			PrintWriter writer = response.getWriter();
+			writer.print("Data: "+id+"님 login ok\nStatus: ");
 	}
-
 }
